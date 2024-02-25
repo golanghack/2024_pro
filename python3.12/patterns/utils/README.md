cbbf3e8eda417750e480d64a86c516694ab56c96
golanghack <golanghack@gmail.com>
2024-02-25 06:31:41 +0300

    ADDED
    Added new program -> python3.12 -> patterns -> utils
    
    This module include a programs for work with patterns
    Low level programming for work with classes

---
+++ python3.12/patterns/utils/README.md
@@ -0,0 +1,1 @@
+## Utils for work in module patterns low level programming
---
+++ python3.12/patterns/utils/report.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+import sys 
+
+def report(message: str, error: bool=False):
+    if len(message) >= 70 and not error:
+        message = message[:67] + '...'
+    sys.stdout.write('\r{:70}{}'.format(message, '\n' if error else ''))
+    sys.stdout.flush()

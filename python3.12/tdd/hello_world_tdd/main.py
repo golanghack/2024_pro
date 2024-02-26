cd86e60d954485d348f9ce1d742e1f84d88cf608
golanghack <golanghack@gmail.com>
2024-02-26 07:20:00 +0300

    REFACTORING

--- python3.12/tdd/hello_world_tdd/main.py
+++ python3.12/tdd/hello_world_tdd/main.py
@@ -1,28 +1,33 @@
-#! /usr/bin/env python3 
+#! /usr/bin/env python3
 
-LANGS = {'Russian': 'Привет, ', 'French': 'Bonjour, ', 'Germany': 'Hallo, ', 'Spanish':'Hola, '}
-DEFAULT = 'Hello, '
+LANGS = {
+    "Russian": "Привет, ",
+    "French": "Bonjour, ",
+    "Germany": "Hallo, ",
+    "Spanish": "Hola, ",
+}
+DEFAULT = "Hello, "
+
 
 def get_prefix(prefix):
     try:
         lang = LANGS[prefix]
-        return lang 
+        return lang
     except KeyError:
         return DEFAULT
-    
-    
+
+
 def hello(name: str, prefix: str) -> str:
-    if name == '':
-        return get_prefix(prefix) + 'World'
+    if name == "":
+        return get_prefix(prefix) + "World"
     return get_prefix(prefix) + name
 
-def main():
-    prefix = get_prefix('')
-    message = hello('', prefix)
-    print(message)
-    
 
-if __name__ == '__main__':
+def main():
+    prefix = get_prefix("")
+    message = hello("", prefix)
+    print(message)
+
+
+if __name__ == "__main__":
     main()
-    
-        

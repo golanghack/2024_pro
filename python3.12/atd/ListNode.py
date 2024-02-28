ac2eab179670658770d337855d0ddc590d04f571
golanghack <golanghack@gmail.com>
2024-02-28 05:10:31 +0300

    ADDED
    Added new program -> python3.12 -> atd -> ListNode
    
    Create node for linked list data structure

---
+++ python3.12/atd/ListNode.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+
+class ListNode:
+    """List Node for LinkedList"""
+
+    def __init__(self, data, link: None) -> None:
+        self.data = data
+        self.link = link

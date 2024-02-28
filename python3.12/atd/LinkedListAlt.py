387a2ca1f071c50d8b2e2a5a1ac8f1ce793ef676
golanghack <golanghack@gmail.com>
2024-02-28 05:38:24 +0300

    ADDED
    Added new programs -> python3.12 -> atd -> LinkedListAlt, LinkedQueue
    
    Realisation Linked queue with linked list

---
+++ python3.12/atd/LinkedListAlt.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python3
+
+from typing import Any
+from ListNode import ListNode
+
+
+class LinkedListAlt:
+    """Create Linked list on python
+    
+    :add_first:         -> add first element 
+    :add_last:          -> add last element
+    :remove_first:      -> remove first element
+    :remove_last:       -> remove last element
+    """
+
+    def __init__(self) -> None:
+        self._head = None
+        self._tail = None
+
+    def add_first(self, item: Any) -> None:
+        """Added fists element in list"""
+
+        self._head = ListNode(item, self._head)
+        if self._tail is None:
+            self._tail = self._head
+
+    def add_last(self, item: Any) -> None:
+        """Added last element in list"""
+        if self._head is None:
+            self.add_first(item)
+        else:
+            self._tail.link = ListNode(item)
+            self._tail = self._tail.link
+
+    def remove_first(self) -> Any:
+        """Remove first element from list"""
+        item = self._head.data
+        self._head = self._head.link
+        if self._head is None:
+            self._tail = None
+        return item
+
+    def remove_last(self) -> Any:
+        """Remove last element from list"""
+
+        if self._head is self._tail:
+            return self.remove_first()
+        else:
+            current = self._head
+            while current.link is not self._tail:
+                current = current.link
+            item = self._tail.data
+            self._tail = current
+            self._tail.link = None
+            return item
---
+++ python3.12/atd/LinkedQueue.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python3
+
+from typing import Any
+from LinkedList import LinkedList
+
+
+class LinkedQueue:
+    """Create Queue with linked list
+    
+    :enqueue:       -> added element in queue
+    :dequeue:       -> remove element from queue
+    :peek:          -> return element from start position
+    :len:           -> return lenght queue
+    :isempty:       -> is empty queue test
+    """
+
+    def __init__(self) -> None:
+        self._list = LinkedList()
+
+    def enqueue(self, item: Any) -> None:
+        """Added element in queue"""
+
+        self._list.add_last(item)
+
+    def dequeue(self) -> Any:
+        """Remove first element"""
+
+        return self._list.remove_first()
+
+    def peek(self) -> Any:
+        """Remove a first element"""
+
+        item = self._list.remove_first()
+        self._list.add_first(item)
+        return item
+
+    def is_empty(self) -> int:
+        """is empty of queue"""
+
+        return len(self) == 0
+
+    def __len__(self) -> int:
+        """Lenght of queue"""
+
+        return len(self._list)

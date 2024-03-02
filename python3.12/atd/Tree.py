a088ba69959b5fdcb5eea7e7712abdfb568b4181
golanghack <golanghack@gmail.com>
2024-03-02 12:57:00 +0300

    REFACTORING
    Changed a program python3.12 -> atd -> Tree
    
    Added new method in class Tree
    Added method __str__ for formation hierarchical structure

--- python3.12/atd/Tree.py
+++ python3.12/atd/Tree.py
@@ -6,3 +6,9 @@ class Tree:
         iterator = iter(_lists)
         self.data = next(iterator)
         self.children = [Tree(elem) for elem in iterator]
+
+    def __str__(self, level=0):
+        tree_string = " " * level + str(self.data)
+        for child in self.children:
+            tree_string += "\n" + child.__str__(level + 1)
+        return tree_string

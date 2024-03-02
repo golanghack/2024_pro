f76e8ae085bef55d766342f6e551de705ff1a3b2
golanghack <golanghack@gmail.com>
2024-03-02 10:10:34 +0300

    ADDED
    Added new program python3.12 -> atd -> lcs_rec
    
    Recursi longest common subsequence algo

---
+++ python3.12/atd/lcs_rec.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+"""Longest common sibsequence"""
+
+
+def lcs_rec(x: str, y: str) -> str:
+    if x == "" or y == "":
+        return ""
+    if x[-1] == y[-1]:
+        return lcs_rec(x[:-1], y[:-1]) + x[-1]
+    else:
+        return max([lcs_rec(x[:-1], y), lcs_rec(x, y[:-1])], key=len)

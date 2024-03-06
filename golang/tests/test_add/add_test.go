c700ab6b246c9591ba30f8ad3a567c5ed340edaa
golanghack <golanghack@gmail.com>
2024-03-06 13:25:03 +0300

    ADDED
    Added new program -> golang -> tests -> add_test
    
    Create tests for program add
    This pgogram use as summator for integers x and y

---
+++ golang/tests/test_add/add_test.go
@@ -0,0 +1,20 @@
+package testadd
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestAdd(t *testing.T) {
+	sum := Add(2, 3)
+	expected := 5
+	if sum != expected {
+			t.Errorf("expected -> '%d' but got '%d'", expected, sum)
+	}
+}
+
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	// output 6
+}

9158c6e66b10a84f6731becfbbec5f77307ca2b9
noskill <golanghack@gmail.com>
2024-04-01 12:23:13 +0300

    CHANGED
    Changed a program -> python3.12 -> django -> app_05 -> blog -> views -> post_list
    
    Added funcionality a pagination

--- python3.12/django/app_05/blog/views.py
+++ python3.12/django/app_05/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from blog.models import Post 
 
 def post_list(request: str):
@@ -7,7 +8,10 @@ def post_list(request: str):
     Get all objects a posts return context, request, dict.
     """
     
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     temp = 'blog/post/list.html'
     context = {'posts': posts,}
     return render(request, temp, context) 

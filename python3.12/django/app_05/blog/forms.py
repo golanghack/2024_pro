8499dcbab9b0105a82f1f794762015db18b5f2cf
noskill <golanghack@gmail.com>
2024-04-01 13:04:31 +0300

    CHANGED
    Chnaged a program -> python3.12 -> django -> app_05 -> blog -> forms
    
    Create docstring for forms email

--- python3.12/django/app_05/blog/forms.py
+++ python3.12/django/app_05/blog/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
 class EmailPostForm(forms.Form):
+    """Email form for sending a post in email
+    
+    Attributes:
+            name: The string for name a user sent email.
+            email: The email for user.
+            to: The email for to email.
+            comments: The string(default len <= 1000 symbols) for email.
+            """
     name = forms.CharField(max_length=30)
     email = forms.EmailField()
     to = forms.EmailField()

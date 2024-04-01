2cb311368a284cf2fbc7a9b7e24d05d076d4177e
noskill <golanghack@gmail.com>
2024-04-01 13:23:29 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_05 -> config -> settings
    
    Added new functionlity -> Email sending with django

--- python3.12/django/app_05/config/settings.py
+++ python3.12/django/app_05/config/settings.py
@@ -136,3 +136,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+
+# console email
+EMAIL_BACKEND = 'django.core.email.backends.console.EmailBackend'

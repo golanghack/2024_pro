b2711642b88375ab22823506d92acc38c499308b
noskill <golanghack@gmail.com>
2024-04-01 17:42:36 +0300

    CHANGED
    Changed a program -> python3.12 -> django -> app_05 -> blog -> urls
    
    added new router for post_share
    
    P.S. formatted a black

--- python3.12/django/app_05/blog/urls.py
+++ python3.12/django/app_05/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from blog.views import post_detail
 from blog.views import post_list
+from blog.views import post_share
 
 app_name = "blog"
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path(
         "<int:year>/<int:month>/<int:day>/<slug:post>/", post_detail, name="post_detail"
     ),
+    path("<int:post_id>/share/", post_share, name="post_share"),
 ]

d11690e609d8595bf361c0ec6254699529c11b2a
noskill <golanghack@gmail.com>
2024-04-03 06:14:02 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_05 -> blog -> templates -> blog -> post
    
    create template a latest_posts for latest post functionality

--- python3.12/django/app_05/blog/templatetags/blog_tags.py
+++ python3.12/django/app_05/blog/templatetags/blog_tags.py
@@ -11,10 +11,10 @@ def total_posts() -> int:
     return Post.published.count()
 
 
-@register.inclusion_tag('blog/post/latest_posts.html')
+@register.inclusion_tag("blog/post/latest_posts.html")
 def show_latest_posts(count=3):
     """REturn last 10 posts in blog."""
-    
-    latest_posts = Post.published.order_by('-publish')[:count]
-    context = {'latest_posts': latest_posts,}
+
+    latest_posts = Post.published.order_by("-publish")[:count]
+    context = {"latest_posts": latest_posts}
     return context

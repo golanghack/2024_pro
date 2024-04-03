14b04db082260fb7023cd3986ad8846ba362b205
noskill <golanghack@gmail.com>
2024-04-03 06:11:22 +0300

    CHANGED
    Changed a program -> python.3.12 -> django -> app_05 -> blog -> templatetags -> blog_tags
    
    Added new functionality -> show latest post (default 3)

--- python3.12/django/app_05/blog/templatetags/blog_tags.py
+++ python3.12/django/app_05/blog/templatetags/blog_tags.py
@@ -9,3 +9,12 @@ def total_posts() -> int:
     """Return a full count a posts published."""
 
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=3):
+    """REturn last 10 posts in blog."""
+    
+    latest_posts = Post.published.order_by('-publish')[:count]
+    context = {'latest_posts': latest_posts,}
+    return context

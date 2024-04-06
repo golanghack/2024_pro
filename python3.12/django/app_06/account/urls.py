978f95aef984816213f69c3be9dfc47f726ef236
noskill <golanghack@gmail.com>
2024-04-06 16:18:39 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> urls
    
    Added new route for  dashboard url

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth
+from account.views import dashboard
 
 
 urlpatterns = [
     path("login/", auth.LoginView.as_view(), name="login"),
     path("logout/", auth.LogoutView.as_view(), name="logout"),
+    path("", dashboard, name="dashboard"),
 ]

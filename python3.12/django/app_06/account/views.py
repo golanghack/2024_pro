571a9a746a1fd9a7c97ddc58711a137d73a1dea7
noskill <golanghack@gmail.com>
2024-04-06 13:38:34 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> views
    
    Added user_login base view in views file, use for uthenticated for user in system

--- python3.12/django/app_06/account/views.py
+++ python3.12/django/app_06/account/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from account.forms import LoginForm
 
-# Create your views here.
+
+def user_login(request):
+    """Return user for this login."""
+
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Success")
+                else:
+                    return HttpResponse("Disabled")
+            else:
+                return HttpResponse("Invalid")
+    else:
+        form = LoginForm()
+    temp = "account/login.html"
+    context = {"form": form}
+    return render(request, temp, context)

c7d4b2c512cdea6185e28059219ba5db211c9cc8
noskill <golanghack@gmail.com>
2024-04-06 16:13:12 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> views
    
    Delete a user_login from views, added dashboard functionality for logged users

--- python3.12/django/app_06/account/views.py
+++ python3.12/django/app_06/account/views.py
@@ -1,29 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import authenticate, login
-from account.forms import LoginForm
 
 
-def user_login(request):
-    """Return user for this login."""
+@login_required
+def dashboard(request):
+    """Full dashboard after logged"""
 
-    if request.method == "POST":
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd["username"], password=cd["password"]
-            )
-            if user is not None:
-                if user.is_active:
-                    login(request, user)
-                    return HttpResponse("Success")
-                else:
-                    return HttpResponse("Disabled")
-            else:
-                return HttpResponse("Invalid")
-    else:
-        form = LoginForm()
-    temp = "account/login.html"
-    context = {"form": form}
-    return render(request, temp, context)
+    template_name = "account/dashboard.html"
+    context = {"section": "dashboard"}
+    return render(request, template_name, context)

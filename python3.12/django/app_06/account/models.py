4d5f30391d4c737c8135b5e11d5044fcca6f770d
noskill <golanghack@gmail.com>
2024-04-07 22:18:16 +0300

    ADDED
    Added new program -> python3.12 -> djngo -> app_06 -> account -> models
    
    Create new model Profile

--- python3.12/django/app_06/account/models.py
+++ python3.12/django/app_06/account/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    """Profile model for user account.
+
+    Attributes:
+            user: A default user model.
+            date_of_birth: date field a birth day of user.
+            photo: A photo for user account.
+    """
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to="/users/%Y/%m/%d/", blank=True)
+
+    def __str__(self) -> str:
+        return f"Profile of {self.user.username}"

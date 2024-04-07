a316ff3ac009b3bd9949ec436260689b9f38a26a
noskill <golanghack@gmail.com>
2024-04-07 22:20:22 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> config -> settings
    
    Added Media files url

--- python3.12/django/app_06/config/settings.py
+++ python3.12/django/app_06/config/settings.py
@@ -158,3 +158,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGIN_URL = "login"
 LOGOUT_URL = "logout"
+
+# media
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'

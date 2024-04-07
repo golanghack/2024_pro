a32b293392c4d14f7032ed027ff10f178610d574
noskill <golanghack@gmail.com>
2024-04-07 22:01:29 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> urls
    
    Added new router for register for new user

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from account.views import dashboard
+from account.views import register
 
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", dashboard, name="dashboard"),
+    path("register/", register, name="register"),
 ]

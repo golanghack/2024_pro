e5483c15440aa94ad66df871f0e2e767e270cb23
noskill <golanghack@gmail.com>
2024-04-07 21:18:46 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> template -> registration
    
    Added new template for password reset

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -21,8 +21,20 @@ urlpatterns = [
         name="password_change_done",
     ),
     # reset
-    path('password-reset/', auth.PasswordResetView.as_view(), name='password_reset'),
-    path('password-reset/done/', auth.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password-reset/<uidb64>/<token>/', auth.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-reset/complete/', auth.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path("password-reset/", auth.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password-reset/done/",
+        auth.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "password-reset/<uidb64>/<token>/",
+        auth.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "password-reset/complete/",
+        auth.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]

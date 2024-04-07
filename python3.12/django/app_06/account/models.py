aaa224a1eeda9676532776a24efe222949c0b9c1
noskill <golanghack@gmail.com>
2024-04-07 22:25:45 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account
    Migrate

--- python3.12/django/app_06/account/models.py
+++ python3.12/django/app_06/account/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
 
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to="/users/%Y/%m/%d/", blank=True)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
 
     def __str__(self) -> str:
         return f"Profile of {self.user.username}"

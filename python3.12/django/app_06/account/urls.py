ad67986e32c03a7edee61b0cfaca846e43826da0
noskill <golanghack@gmail.com>
2024-04-07 21:39:32 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> urls
    
    Delete custom routers for auth, added standart auth urls from django

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -1,40 +1,8 @@
-from django.urls import path
-from django.contrib.auth import views as auth
+from django.urls import path, include
 from account.views import dashboard
 
 
 urlpatterns = [
-    # auth
-    path("login/", auth.LoginView.as_view(), name="login"),
-    path("logout/", auth.LogoutView.as_view(), name="logout"),
-    # main
+    path("", include("django.contrib.auth.urls")),
     path("", dashboard, name="dashboard"),
-    # change a pass/log
-    path(
-        "password-change/",
-        auth.PasswordChangeView.as_view(),
-        name="password_change",
-    ),
-    path(
-        "password-change/done/",
-        auth.PasswordChangeDoneView.as_view(),
-        name="password_change_done",
-    ),
-    # reset
-    path("password-reset/", auth.PasswordResetView.as_view(), name="password_reset"),
-    path(
-        "password-reset/done/",
-        auth.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "password-reset/<uidb64>/<token>/",
-        auth.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "password-reset/complete/",
-        auth.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
 ]

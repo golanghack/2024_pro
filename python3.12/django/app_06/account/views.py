5afed96974f47fbdf90482725e0e8a24956708ad
noskill <golanghack@gmail.com>
2024-04-08 18:36:36 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> views
    
    Added new view for editing a profile of user

--- python3.12/django/app_06/account/views.py
+++ python3.12/django/app_06/account/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from account.forms import LoginForm, UserRegistrationForm
+from account.forms import UserRegistrationForm
+from account.forms import UserEditForm
+from account.forms import ProfileEditForm
 from account.models import Profile
 
 
@@ -36,5 +38,28 @@ def register(request):
     template_name = "account/register.html"
     context = {
         "user_form": user_form,
+    }
+    return render(request, template_name, context)
+
+
+@login_required
+def edit(request):
+    """Editing a profile of user."""
+
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+    template_name = "account/edit.html"
+    context = {
+        "user_form": user_form,
+        "profile_form": profile_form,
     }
     return render(request, template_name, context)

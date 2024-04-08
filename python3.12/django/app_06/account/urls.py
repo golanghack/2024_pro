0afff89bcb368034cb56006a8c25992262b8b8b2
noskill <golanghack@gmail.com>
2024-04-08 18:42:36 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> urls
    
    Create new router for edit a profile

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from account.views import dashboard
 from account.views import register
+from account.views import edit
 
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("", dashboard, name="dashboard"),
     path("register/", register, name="register"),
+    path("edit/", edit, name="edit"),
 ]

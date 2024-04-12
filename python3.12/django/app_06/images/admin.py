0f904f29188689dbcec5957e17d4a77a9a27392a
noskill <golanghack@gmail.com>
2024-04-12 21:13:34 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images
    
    Added new model Image in admin panel

--- python3.12/django/app_06/images/admin.py
+++ python3.12/django/app_06/images/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from images.models import Image
 
-# Register your models here.
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    """Image Admin pannel"""
+    
+    list_display = [
+        'title',
+        'slug',
+        'image',
+        'created',
+    ]
+    list_filter = [
+        'created',
+    ]

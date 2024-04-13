85a6bcb4bc72c1d3a04fe1557a3177ab07356210
noskill <golanghack@gmail.com>
2024-04-13 13:24:37 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images -> urls
    
    Added new router for detail info about image

--- python3.12/django/app_06/images/urls.py
+++ python3.12/django/app_06/images/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from images.views import image_create
+from images.views import image_detail
 
 app_name = "images"
 
 urlpatterns = [
     path("create/", image_create, name="create"),
+    path("detail/<int:id>/<slug:slug>/", image_detail, name="detail"),
 ]

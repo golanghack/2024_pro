231c3253688da6a38458a3a2d6c33a4505f0110d
noskill <golanghack@gmail.com>
2024-04-13 16:29:12 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> urls
    
    Added routers for user detail and user list

--- python3.12/django/app_06/account/urls.py
+++ python3.12/django/app_06/account/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from account.views import dashboard
 from account.views import register
 from account.views import edit
+from account.views import user_list
+from account.views import user_detail
 
 
 urlpatterns = [
@@ -9,4 +11,6 @@ urlpatterns = [
     path("", dashboard, name="dashboard"),
     path("register/", register, name="register"),
     path("edit/", edit, name="edit"),
+    path("users/", user_list, name="user_list"),
+    path("users/<username>/", user_detail, name="user_detail"),
 ]

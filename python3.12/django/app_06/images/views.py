f08bd3d20c695d71337cddce9ed0c9321fa608bc
noskill <golanghack@gmail.com>
2024-04-13 12:34:59 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images -> views
    
    Added new view for save images in site

--- python3.12/django/app_06/images/views.py
+++ python3.12/django/app_06/images/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from images.forms import ImageCreateForm
 
-# Create your views here.
+
+@login_required
+def image_create(request):
+    """View for creation a image link."""
+
+    if request.method == "POST":
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+        new_image.user = request.user
+        new_image.save()
+        messages.success(request, "Image added")
+        return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(data=request.GET)
+    template_name = "images/image/create.html"
+    context = {
+        "section": "images",
+        "form": form,
+    }
+    return render(request, template_name, context)

670fc68ae86beecfd4c6ac1b1263ed24e014757e
noskill <golanghack@gmail.com>
2024-04-13 13:26:52 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images -> models -> Image
    
    Added get_absolute_url for canonical url

--- python3.12/django/app_06/images/models.py
+++ python3.12/django/app_06/images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -51,3 +52,7 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        context = "images:detail"
+        return reverse(context, args=[self.id, self.slug])

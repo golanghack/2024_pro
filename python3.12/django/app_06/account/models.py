f96edfec07451851638ffb10ca3ffa8236eb2ec8
noskill <golanghack@gmail.com>
2024-04-13 15:38:44 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> account -> models
    
    Added new model Contact for following user to user

--- python3.12/django/app_06/account/models.py
+++ python3.12/django/app_06/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
@@ -17,3 +18,43 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f"Profile of {self.user.username}"
+
+
+class Contact(models.Model):
+    """Model for Contact profile.
+
+    Attributes:
+            user_from: An user from.
+            user_to: An user to.
+            created: Date a formation net.
+    """
+
+    user_from = models.ForeignKey(
+        "auth.User", related_name="related_from_set", on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        "auth.User", related_name="related_to_set", on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["-created"]),
+        ]
+
+    ordering = [
+        "-created",
+    ]
+
+    def __str__(self):
+        return f"{self.user_from} follow {self.user_to}"
+
+
+# dinamicaly added filed in User model
+user_model = get_user_model()
+user_model.add_to_class(
+    "following",
+    models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    ),
+)

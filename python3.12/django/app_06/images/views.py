168a81bda948db92bb04bf5a621b13bbf2470738
noskill <golanghack@gmail.com>
2024-04-15 20:15:52 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images -> views
    
    Added function create_action in users liked

--- python3.12/django/app_06/images/views.py
+++ python3.12/django/app_06/images/views.py
@@ -58,6 +58,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == "like":
                 image.users_like.add(request.user)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
             context = {

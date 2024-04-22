bef271878318357ff72790e55c1007e69954259a
noskill <golanghack@gmail.com>
2024-04-22 20:30:00 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_06 -> images -> urls
    
    Added router for rank of images

--- python3.12/django/app_06/images/urls.py
+++ python3.12/django/app_06/images/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from images.views import image_create
 from images.views import image_detail
 from images.views import image_like
+from images.views import image_ranking
 
 app_name = "images"
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path("create/", image_create, name="create"),
     path("detail/<int:id>/<slug:slug>/", image_detail, name="detail"),
     path("like/", image_like, name="like"),
+    path('rank/', image_ranking, name='rank'),
 ]

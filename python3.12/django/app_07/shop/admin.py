e89709b2bc87a56919fc68625c7f08cf6d804bff
noskill <golanghack@gmail.com>
2024-04-27 21:07:27 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_07 -> shop -> admin
    
    Added models Category, Product in admin panel

--- python3.12/django/app_07/shop/admin.py
+++ python3.12/django/app_07/shop/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from shop.models import Product
+from shop.models import Category
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+    ]
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "slug",
+        "price",
+        "available",
+        "created",
+        "updated",
+    ]
+    list_filter = [
+        "available",
+        "created",
+        "updated",
+    ]
+    list_editable = [
+        "price",
+        "available",
+    ]
+    prepopulated_fields = {
+        "slug": ("name",),
+    }

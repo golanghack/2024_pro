648aedec4b9c9fcd876716a355ca26aaeea15d8b
noskill <golanghack@gmail.com>
2024-04-28 20:01:57 +0300

    ADDED
    Added new program -> python3.12 -> django -> app_07 -> views
    
    Create new view for get product list

--- python3.12/django/app_07/shop/views.py
+++ python3.12/django/app_07/shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from shop.models import Product
+from shop.models import Category
 
-# Create your views here.
+
+def get_product_list(request: str, category_clug: str = None):
+    """The view for product list"""
+
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_clug)
+        products = products.filter(categories=category)
+    template_name = "shop/product/list.html"
+    context = {
+        "category": category,
+        "categories": categories,
+        "products": products,
+    }
+    return render(request, template_name, context)

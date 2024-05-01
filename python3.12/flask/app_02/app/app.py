1b0508f142e5d3a781bfde1757b19c241062775b
noskill <golanghack@gmail.com>
2024-05-01 18:40:46 +0300

    ADDED
    Added new program -> golang -> tools

--- python3.12/flask/app_02/app/app.py
+++ python3.12/flask/app_02/app/app.py
@@ -1,9 +1,11 @@
+from flask_script import Manager 
 from flask import Flask 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/user/<username>')
 def index(username: str) -> str:
     return f'<h1>Hello {username}</h2>'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
